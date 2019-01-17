d553adb92724d38ebd59642e163486ddb33432c1
Lucas Couto <loc.unb@gmail.com>
2019-01-18 01:12:37 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+
+import { AngularFireModule } from "@angular/fire";
+import {
+  AngularFireDatabaseModule,
+  AngularFireDatabase
+} from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBthU9q7uf48uH3_P0pMDEThgbE98PR-DA",
+  authDomain: "ibusiness-test-fe6c7.firebaseapp.com",
+  databaseURL: "https://ibusiness-test-fe6c7.firebaseio.com",
+  projectId: "ibusiness-test-fe6c7",
+  storageBucket: "ibusiness-test-fe6c7.appspot.com",
+  messagingSenderId: "1001140091098"
+};
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from "@angular/core";
+import { NavController, ToastController } from "ionic-angular";
+
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Observable } from "rxjs/Observable";
+import { AngularFirestore } from "angularfire2/firestore";
+import { registerLocaleData } from "@angular/common";
+import { INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from "@angular/platform-browser-dynamic/src/platform_providers";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  userDoc: any;
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild("barcode") barcode;
+  @ViewChild("description") description;
+  @ViewChild("name") name;
+
+
+
+  constructor(public navCtrl: NavController, public afDB: AngularFireDatabase, private fireStore: AngularFirestore, public toastCtrl: ToastController) {
 
   }
 
+  register() {
+
+    let toast = this.toastCtrl.create({ duration: 3000, position: "bottom" });
+
+    console.log(this.barcode.value);
+
+    this.userDoc = this.fireStore.collection<any>('products').add({
+      barcode: this.barcode.value,
+      description: this.description.value,
+      name: this.name.value,
+    });
+
+
+
+    toast.setMessage("Cadastrado com sucesso!");
+
+    toast.present();
+  }
 }
